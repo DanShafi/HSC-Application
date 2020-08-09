183fe36587a17e46e7659a7afbef43e4c6491ef8
Danish Shafi <danish.shafi@live.co.uk>
2020-08-09 01:17:43 +0100

    Social icons added to Team components

--- src/components/Team.js
+++ src/components/Team.js
@@ -7,7 +7,24 @@ import M1 from '../media/m1.png'
 import H1 from '../media/h1.png'
 import G1 from '../media/g1.png'
 import D1 from '../media/d1.png'
-import githubLogo from '../media/github_logo.svg'
+
+// Images Import
+import A1Mail from '../media/a1_mail_icon.svg'
+import A1Twit from '../media/a1_twitter_icon.svg'
+import A1Insta from '../media/a1_insta_icon.svg'
+
+import M1Mail from '../media/m1_mail_icon.svg'
+import M1Twit from '../media/m1_twitter_icon.svg'
+import M1Git from '../media/m1_github_icon.svg'
+
+import H1Mail from '../media/h1_mail_icon.svg'
+
+import G1Mail from '../media/g1_mail_icon.svg'
+import G1Insta from '../media/g1_insta_icon.svg'
+
+import D1Mail from '../media/d1_mail_icon.svg'
+import D1Twit from '../media/d1_twitter_icon.svg'
+import D1Git from '../media/d1_github_icon.svg'
 
 const center = {
     textAlign: 'center'
@@ -20,63 +37,122 @@ const imgHeight = {
 const socialIcons = {
     display: 'flex',
     listStyleType: 'none',
+    justifyContent: 'space-evenly',
+    marginRight: '50px',
+    marginTop: '-15px',
 }
-
 
 const Team = () => {
     return (
         <Container>
             <Row>
                 <Col style={center}>
-                    <img className="team-head" src={A1} style={imgHeight} />
+                    <img className="team-head" src={A1} style={imgHeight} alt="Team-Member-Head" />
                     <div>
                         <b>Andy Foster</b>
                         <p>Illustrator & Animator</p>
                         <ul style={socialIcons}>
-                            <li>
+                            <li className="socialIcons">
                                 <a href="google.com">
-                                    <img src={githubLogo} alt="Github-logo" />
+                                    <img src={A1Mail} alt="Mail-Icon" />
                                 </a>
                             </li>
-                            <li>2</li>
-                            <li>3</li>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={A1Twit} alt="Twitter-Icon" />
+                                </a>
+                            </li>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={A1Insta} alt="Instagram-Icon" />
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </Col>
 
                 <Col style={center}>
-                    <img className="team-head" src={M1} style={imgHeight}/>
+                    <img className="team-head" src={M1} style={imgHeight} alt="Team-Member-Head"/>
                     <div>
                         <b>Martin O'Dea</b>
                         <p>Web Designer & Developer</p>
-                        <span></span>
+                        <ul style={socialIcons}>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={M1Mail} alt="Mail-Icon" />
+                                </a>
+                            </li>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={M1Twit} alt="Twitter-Icon" />
+                                </a>
+                            </li>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={M1Git} alt="Github-Icon" />
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </Col>
 
                 <Col style={center}>
-                    <img className="team-head" src={H1} style={imgHeight}/>
+                    <img className="team-head" src={H1} style={imgHeight} alt="Team-Member-Head"/>
                     <div>
                         <b>Hannah Kirkman</b>
                         <p>Account Director</p>
-                        <span></span>
+                        <ul style={socialIcons}>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={H1Mail} alt="Mail-Icon" />
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </Col>
 
                 <Col style={center}>
-                    <img className="team-head" src={G1} style={imgHeight}/>
+                    <img className="team-head" src={G1} style={imgHeight} alt="Team-Member-Head"/>
                     <div>
                         <b>George Williams</b>
                         <p>Animator</p>
-                        <span></span>
+                        <ul style={socialIcons}>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={G1Mail} alt="Mail-Icon" />
+                                </a>
+                            </li>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={G1Insta} alt="Instagram-Icon" />
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </Col> 
 
                 <Col style={center}>
-                    <img className="team-head" src={D1} style={imgHeight}/>
+                    <img className="team-head" src={D1} style={imgHeight} alt="Team-Member-Head"/>
                     <div>
                         <b>Danish Shafi</b>
                         <p>Web Developer</p>
-                        <span></span>
+                        <ul style={socialIcons}>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={D1Mail} alt="Mail-Icon" />
+                                </a>
+                            </li>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={D1Twit} alt="Twitter-Icon" />
+                                </a>
+                            </li>
+                            <li className="socialIcons">
+                                <a href="google.com">
+                                    <img src={D1Git} alt="Github-Icon" />
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </Col>
             </Row>
